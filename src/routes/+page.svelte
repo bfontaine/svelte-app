cc03de90b8692708df6ca93d8db7979a50499189
Baptiste Fontaine <baptiste@bixoto.com>
2023-12-27 09:30:44 +0000

    simplify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-
 <!-- data-sveltekit-reload to prevent Svelte from trying to navigate to /search in JS -->
 <form action="/search" data-sveltekit-reload>
-  <input name="q" type="search" aria-label="Search" />
+  <input name="q" type="search" />
   <input type="submit" value="Search" />
 </form>
